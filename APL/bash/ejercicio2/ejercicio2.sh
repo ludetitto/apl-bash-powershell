421152bf511f98ab01f1734f82397e41580514ce
samuui1 <elitete23153@gmail.com>
2026-04-25 16:25:07 +0000

    elimino espacios antes y despues de signo

--- APL/bash/ejercicio2/ejercicio2.sh
+++ APL/bash/ejercicio2/ejercicio2.sh
@@ -1,27 +1,29 @@
 #!/bin/sh
 
 normalizar(){
-	texto="$1"
-	texto=$(echo "$texto" | tr '[:upper:]' '[:lower:]')
-	texto=$(echo "$texto" | sed -E 's/ +([.,;:?!])/\1/g')
-	echo "$texto"
+	archivo="$1"
+	sed -E '
+	s/ +/ /g;
+	s/^ //;
+	s/ $//;
+	s/ ([.,;.?!])/\1/g;
+	s/\.{4,}/.../g;
+	' "$archivo"
 }
 
+# 1. Validar cantidad de parámetros
 if [ "$#" -ne 1 ]; then
-	echo "No uso 1 parametro"
-	exit 1
+        echo "Uso: $0 <archivo>"
+        exit 1
 fi
 
 ARCHIVO=$1
 
+# 2. Validar que el archivo exista
 if [ ! -f "$ARCHIVO" ]; then
-	echo "No se paso un archivo"
-	exit 1
+        echo "Error: El archivo '$ARCHIVO' no existe."
+        exit 1
 fi
 
-while read -r linea
-do
-	normalizada=$(normalizar "$linea")
-	echo "$normalizada"
-	echo "$linea"
-done < "$ARCHIVO"
+# 3. Llamar a la función
+normalizar "$ARCHIVO"
