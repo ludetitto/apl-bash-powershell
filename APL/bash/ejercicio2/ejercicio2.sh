0274f994be175e037fbaad3224844888cc37a9b1
samuui1 <elitete23153@gmail.com>
2026-04-28 00:22:30 +0000

    Agrego signo al final del parrago si hay alguno sin cerrar e inicializo en mayuscula despues de signos

--- APL/bash/ejercicio2/ejercicio2.sh
+++ APL/bash/ejercicio2/ejercicio2.sh
@@ -8,7 +8,54 @@ normalizar(){
 	s/ $//;
 	s/ ([.,;.?!])/\1/g;
 	s/\.{4,}/.../g;
-	' "$archivo"
+	' "$archivo" |
+	awk 'BEGIN { RS = ""; ORS = "\n\n" }
+	{
+	    #Limpio espacios o saltos de línea al final
+	    sub(/[ \t\n]+$/, "")
+
+	    #Si encuentra un "¡" y ningun "!" después de él
+	    if ($0 ~ /¡[^!]*$/) {
+	        sub(/[.!?]*$/, "")  # Borra el punto o error que haya al final
+	        $0 = $0 "!"         # Le pone la exclamación
+	    }
+	    #Si encuentra un "¿" y ningun "?" después de él
+	    else if ($0 ~ /¿[^?]*$/) {
+	        sub(/[.!?]*$/, "")  
+	        $0 = $0 "?"
+	    }
+	    #Si todo estaba bien cerrado, verifico que tenga punto final
+	    else if ($0 !~ /[.!?]$/ && length($0) > 0) {
+	        $0 = $0 "."
+	    }
+
+	    print $0
+	}' | 
+	awk 'BEGIN { 
+      	    mayus = 1 
+	}
+	{
+	    linea = ""
+	    if (length($0) == 0) {
+	        mayus = 1
+	        print ""
+	        next
+	    }
+	    for (i = 1; i <= length($0); i++) {
+	        c = substr($0, i, 1)
+	        if (mayus == 1 && c ~ /[a-zA-ZáéíóúÁÉÍÓÚñÑ]/) {
+	            linea = linea toupper(c)
+	            mayus = 0
+	        } 
+	        else {
+	            linea = linea c
+	            if (c ~ /[.!?]/) {
+	                mayus = 1
+	            }
+	        }
+	    }
+	    print linea
+	}' > texto_corregido.txt	
 }
 
 # 1. Validar cantidad de parámetros
