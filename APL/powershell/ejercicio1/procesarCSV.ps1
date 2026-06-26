4448864d8875eb2e147fea321f06411e774774fe
ramimedx <ramiroagustinmedina@gmail.com>
2026-06-25 21:48:31 -0300

    .

--- APL/powershell/ejercicio1/procesarCSV.ps1
+++ APL/powershell/ejercicio1/procesarCSV.ps1
@@ -72,24 +72,6 @@ param (
     [switch]$Contar
 )
 
-[CmdletBinding(PositionalBinding=$false)]
-param (
-    [Alias('a')]
-    [string]$archivo,
-
-    [Alias('f')]
-    [string]$filtro,
-
-    [Alias('b')]
-    [string]$buscar,
-
-    [Alias('s')]
-    [string]$sumar,
-
-    [Alias('c')]
-    [switch]$contar
-)
-
 # =========================
 # Funciones
 # =========================
